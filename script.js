3b514886b2677adb3289fe6dd768009c3c06b835
tanu576 <65331244+tanu576@users.noreply.github.com>
2021-01-03 01:10:16 -0700

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+scrollbar = document.querySelector(".scroll-bar");
+scrollbar.addEventListener("click",myFunction);
+function myFunction(){
+	document.querySelector(".navbar").classList.toggle("show");
+})
